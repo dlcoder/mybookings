8964ea1816c57761572e8ea2239e980a7bf374c5
Jesús Manuel García Muñoz <skuark@gmail.com>
2014-04-01 19:30:50 +0200

    Automatically migrate the test schema.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -14,9 +14,8 @@ Dir[Rails.root.join("spec/support/**/*.rb")].each { |f| require f }
 # Turnip
 Dir.glob("spec/steps/**/*steps.rb") { |f| load f, true }
 
-# Checks for pending migrations before tests are run.
-# If you are not using ActiveRecord, you can remove this line.
-ActiveRecord::Migration.check_pending! if defined?(ActiveRecord::Migration)
+# Rails 4.1 maintains the test schema automatically
+ActiveRecord::Migration.maintain_test_schema!
 
 RSpec.configure do |config|
   # ## Mock Framework
