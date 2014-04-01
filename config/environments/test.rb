c2f20eae79a7a838efb9bbd4655c2a50a453a84d
Jesús Manuel García Muñoz <skuark@gmail.com>
2014-04-01 19:38:31 +0200

    Changed default_url_options.

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -38,5 +38,5 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   # Required by Devise
-  config.action_mailer.default_url_options = { host: 'hivitae.dev' }
+  config.action_mailer.default_url_options = { host: 'mybookings.dev' }
 end
