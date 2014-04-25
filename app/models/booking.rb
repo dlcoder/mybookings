559afb2712931d473ee18889dedd7c5b867af116
Jesús Manuel García Muñoz <skuark@gmail.com>
2014-04-25 20:58:15 +0200

    Booking dates validation.

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -4,6 +4,8 @@ class Booking < ActiveRecord::Base
 
   validates :resource, :start_date, :end_date, presence: true
   validate :resource_exists
+  validate :date_in_the_future
+  validate :date_range
 
   delegate :name, to: :resource, prefix: true
 
@@ -24,5 +26,24 @@ class Booking < ActiveRecord::Base
 
   def resource_exists
     errors.add(:resource_id, I18n.t('errors.messages.blank')) if resource.nil?
+  end
+
+  def date_in_the_future
+    unless start_date.nil?
+      errors.add(:start_date, I18n.t('errors.messages.booking.start_date_in_the_past')) if start_date.past?
+    end
+
+    unless end_date.nil?
+      errors.add(:end_date, I18n.t('errors.messages.booking.end_date_in_the_past')) if end_date.past?
+    end
+  end
+
+  def date_range
+    unless start_date.nil? or end_date.nil?
+      if end_date <= start_date
+        errors.add(:start_date, I18n.t('errors.messages.booking.start_date_greater_than_end_date'))
+        errors.add(:end_date, I18n.t('errors.messages.booking.end_date_smaller_than_start_date'))
+      end
+    end
   end
 end
--- spec/models/booking_spec.rb
+++ spec/models/booking_spec.rb
@@ -1,4 +1,29 @@
 require 'spec_helper'
 
 describe Booking do
+  let(:booking) { Booking.new(resource: Resource.new, start_date: 1.hour.from_now, end_date: 2.hour.from_now) }
+
+  it 'validates that a booking with start date in the past is not valid' do
+    booking.start_date = 2.hour.ago
+
+    expect(booking.valid?).to be_false
+    expect(booking).to have(1).errors_on(:start_date)
+  end
+
+  it 'validates that a booking with end date in the past is not valid' do
+    booking.start_date = 2.hour.ago
+    booking.end_date = 1.hour.ago
+
+    expect(booking.valid?).to be_false
+    expect(booking).to have(1).errors_on(:end_date)
+  end
+
+  it 'validates that a booking with start date greater than end date is not valid' do
+    booking.start_date = 2.day.from_now
+    booking.end_date = 1.day.from_now
+
+    expect(booking.valid?).to be_false
+    expect(booking).to have(1).errors_on(:start_date)
+    expect(booking).to have(1).errors_on(:end_date)
+  end
 end
