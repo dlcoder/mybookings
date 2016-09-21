c51a83c9c54bab840f70ac2b12d8996fe3dc2492
Jose Manuel Martinez Gallardo <jose@deliriumcoder.com>
2016-09-21 12:24:54 +0200

    Changes remaining bookings mailer references.

--- app/mailers/notifications_mailer.rb
+++ app/mailers/notifications_mailer.rb
@@ -2,11 +2,11 @@ class NotificationsMailer < ApplicationMailer
   def cancel_booking booking, reason
     @booking = booking
     @reason = reason
-    mail(to: booking.user_email, subject: t('bookings_mailer.cancel_booking.subject'))
+    mail(to: booking.user_email, subject: t('notifications_mailer.cancel_booking.subject'))
   end
 
   def notify_upcoming_booking booking
     @booking = booking
-    mail(to: booking.user_email, subject: t('bookings_mailer.notify_upcoming_booking.subject'))
+    mail(to: booking.user_email, subject: t('notifications_mailer.notify_upcoming_booking.subject'))
   end
 end
