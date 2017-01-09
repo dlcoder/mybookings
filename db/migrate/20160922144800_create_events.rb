0522a7d037777fe746c24ba6bc291d29d8cd8412
Jose Manuel Martinez Gallardo <jose@deliriumcoder.com>
2017-01-09 16:25:08 +0100

    Group related lines.

--- db/migrate/20160922144800_create_events.rb
+++ db/migrate/20160922144800_create_events.rb
@@ -2,28 +2,20 @@ class CreateEvents < ActiveRecord::Migration
   def change
 
     create_table :events do |t|
-      t.datetime :start_date
-      t.datetime :end_date
+      t.datetime    :start_date
+      t.datetime    :end_date
+      t.integer     :status,    null: false, default: 0
+      t.text        :feedback
+      t.belongs_to  :booking,   index: true
+      t.belongs_to  :resource,  index: true
     end
 
-    add_reference :events, :booking, index: true
-
-    remove_column :bookings, :start_date, :datetime
-    remove_column :bookings, :end_date, :datetime
-
-    remove_reference :bookings, :resource, index: true
-
-    add_reference :events, :resource, index: true
-
-    add_reference :bookings, :resource_type, index: true
-
-    remove_column :bookings, :status, :integer
-
-    add_column :events, :status, :integer, null: false, default: 0
-
-    remove_column :bookings, :feedback, :text
-
-    add_column :events, :feedback, :text
+    remove_column     :bookings, :start_date,     :datetime
+    remove_column     :bookings, :end_date,       :datetime
+    remove_reference  :bookings, :resource,       index: true
+    add_reference     :bookings, :resource_type,  index: true
+    remove_column     :bookings, :status,         :integer
+    remove_column     :bookings, :feedback,       :text
 
   end
 end
