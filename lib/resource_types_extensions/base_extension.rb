59133e691ec1333f8c03a9c78c3e86834fa6d9d0
Jesús Manuel García Muñoz <jesus@deliriumcoder.com>
2017-01-11 19:23:29 +0100

    Revised base extension method names.

--- lib/resource_types_extensions/base_extension.rb
+++ lib/resource_types_extensions/base_extension.rb
@@ -2,10 +2,10 @@ class ResourceTypesExtensions::BaseExtension
   def self.after_booking_creation event
   end
 
-  def self.on_booking_start event
+  def self.on_event_start event
   end
 
-  def self.on_booking_end event
+  def self.on_event_end event
   end
 
   def self.actions_for event
