29a83ed39e42af40b352befc4df8d513f4a34703
Jesús Manuel García Muñoz <jesus@deliriumcoder.com>
2017-01-11 19:21:39 +0100

    Minor seeds correction.

--- db/seeds.rb
+++ db/seeds.rb
@@ -4,7 +4,7 @@ admin.roles = [:admin]
 admin.save!
 
 resource_type_vp = ResourceType.create({ name: 'Virtual PC', extension: 'DefaultExtension' })
-resource_type_acmr = ResourceType.create({ name: 'Adobe Connect Meeting Room', extension: 'AdobeConnectExtension' })
+resource_type_acmr = ResourceType.create({ name: 'Adobe Connect Meeting Room', extension: 'DefaultExtension' })
 
 resources = Resource.create([
   { name: 'PC1', resource_type: resource_type_vp },
