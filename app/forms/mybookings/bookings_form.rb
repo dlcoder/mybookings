7894970fb1d41fbd8c253426a1ab1996bac8ab51
Jose Manuel Martinez Gallardo <jose@deliriumcoder.com>
2017-01-23 12:06:27 +0100

    Validates the duration of an event with an application configuration.

--- app/forms/mybookings/bookings_form.rb
+++ app/forms/mybookings/bookings_form.rb
@@ -9,6 +9,7 @@ module Mybookings
     validates :resource, :start_date, :end_date, presence: true
     validates :until_date, presence: true, if: :is_an_recurring_booking?
     validate :the_booking_period_is_valid, if: :is_an_recurring_booking?
+    validate :the_event_duration_is_valid
 
     def initialize params={}
       self.resource = params[:resource]
@@ -40,7 +41,15 @@ module Mybookings
       booking_interval = until_date.to_datetime - start_date.to_datetime
       range_permitted = MYBOOKINGS_CONFIG['maximum_permitted_days_for_recurring_events']
       if  range_permitted.days - booking_interval.days < 0
-        errors.add(:until_date, I18n.t('.bookings.new_booking_events_step.dates_interval_message_error', days_permitted: range_permitted))
+        errors.add(:until_date, I18n.t('.mybookings.bookings.new_booking_events_step.dates_interval_message_error', days_permitted: range_permitted))
+      end
+    end
+
+    def the_event_duration_is_valid
+      event_duration_in_seconds = end_date.to_time - start_date.to_time
+      permitted_event_duration_in_seconds = MYBOOKINGS_CONFIG['maximum_duration_in_hours_for_an_event'] * 3600
+      if event_duration_in_seconds > permitted_event_duration_in_seconds
+        errors.add(:base, I18n.t('.mybookings.bookings.new_booking_events_step.event_duration_message_error', event_duration: MYBOOKINGS_CONFIG['maximum_duration_in_hours_for_an_event']))
       end
     end
 
