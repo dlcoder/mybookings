f16b5ce904c703ceef1c770023011ac73b291a0a
Jesús Manuel García Muñoz <skuark@gmail.com>
2017-02-11 16:35:35 +0100

    test

--- spec/dummy/db/seeds.rb
+++ spec/dummy/db/seeds.rb
@@ -3,8 +3,8 @@ admin = Mybookings::User.create({ email: 'user@example.com', password: '12345678
 admin.roles = [:admin]
 admin.save!
 
-resource_type_vp = Mybookings::ResourceType.create({ name: 'Virtual PC', extension: 'DefaultExtension' })
-resource_type_acmr = Mybookings::ResourceType.create({ name: 'Adobe Connect Meeting Room', extension: 'DefaultExtension' })
+resource_type_vp = Mybookings::ResourceType.create({ name: 'Virtual PC', extension: 'DefaultExtension', roles_mask: 0 })
+resource_type_acmr = Mybookings::ResourceType.create({ name: 'Adobe Connect Meeting Room', extension: 'DefaultExtension', roles_mask: 14 })
 
 resources = Mybookings::Resource.create([
   { name: 'PC1', resource_type: resource_type_vp },
