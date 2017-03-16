17e8c03a61d6afee41d30bc0ea49af50bd7aee43
Jesús Manuel García Muñoz <jesus@deliriumcoder.com>
2017-03-16 21:38:23 +0100

    Revised notifications mailer template for new bookings, and simplify the others notifications mailer templates.

--- app/mailers/mybookings/notifications_mailer.rb
+++ app/mailers/mybookings/notifications_mailer.rb
@@ -6,7 +6,7 @@ module Mybookings
 
       notification_subject = t('mybookings.notifications_mailer.cancel_event.subject')
 
-      mail(from: notifications_email_from(event), to: event.booking_user_email, subject: "[#{app_name}] #{notification_subject}")
+      mail(from: from_email_for(event), to: event.booking_user_email, subject: "[#{app_name}] #{notification_subject}")
     end
 
     def notify_upcoming_booking booking
@@ -14,19 +14,22 @@ module Mybookings
 
       notification_subject = t('mybookings.notifications_mailer.notify_upcoming_booking.subject')
 
-      mail(from: notifications_email_from(booking), to: booking.user_email, subject: "[#{app_name}] #{notification_subject}")
+      mail(from: from_email_for(booking), to: booking.user_email, subject: "[#{app_name}] #{notification_subject}")
     end
 
     def notify_new_booking booking
-      emails = booking.resource_type_notifications_emails
-      return if emails.empty?
+      destination_emails = booking.resource_type_notifications_emails
+      return if destination_emails.empty?
 
-      @resource_type_name = booking.resource_type_name
+      @params = params_for_new_booking_notification(booking)
 
-      notification_subject = t('mybookings.notifications_mailer.notify_new_booking.subject')
+      subject = "[#{app_name}] #{t('mybookings.notifications_mailer.notify_new_booking.subject',
+        resource_type_name: @params[:resource_type_name],
+        user_email: @params[:user_email]
+      )}"
 
-      emails.each do |email|
-        mail(from: notifications_email_from(booking), to: email, subject: "[#{app_name}] #{notification_subject}")
+      destination_emails.each do |email|
+        mail(from: from_email_for(booking), to: email, subject: subject)
       end
     end
 
@@ -39,13 +42,13 @@ module Mybookings
       notification_subject = t('mybookings.notifications_mailer.notify_delete_booking.subject')
 
       emails.each do |email|
-        mail(from: notifications_email_from(booking), to: email, subject: "[#{app_name}] #{notification_subject}")
+        mail(from: from_email_for(booking), to: email, subject: "[#{app_name}] #{notification_subject}")
       end
     end
 
     private
 
-    def notifications_email_from resource
+    def from_email_for resource
       email = resource.resource_type_notifications_email_from if resource.is_a? Booking
       email = resource.booking_resource_type_notifications_email_from if resource.is_a? Event
 
@@ -55,6 +58,28 @@ module Mybookings
 
     def app_name
       t('mybookings.app_name')
+    end
+
+    def params_for_new_booking_notification booking
+      params_events = []
+
+      booking.events.each do |event|
+        params_events.push({
+          resource_name: event.resource_name,
+          start_date: event.start_date,
+          end_date: event.end_date
+        })
+      end
+
+      params = {
+        user_email: booking.user_email,
+        resource_type_name: booking.resource_type_name,
+        booking_date: booking.created_at,
+        comment: booking.comment,
+        events: params_events
+      }
+
+      params
     end
   end
 end
